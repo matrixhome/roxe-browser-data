0b0589222e67f1b348d3008616c1aa3131eaa0c8
kevin.h <bikaihuang@jiujiutech.cn>
2021-05-12 11:18:03 +0800

    update

--- service/tools/mysql.js
+++ service/tools/mysql.js
@@ -3,7 +3,7 @@ var mysql = require('promise-mysql');
 MysqlPool = mysql.createPool({
     host: '127.0.0.1',
     user: 'exone',
-    password: 'Roxe.6G!',
+    password: '123456',
     database: 'roxe',
     connectionLimit: 30
 });
